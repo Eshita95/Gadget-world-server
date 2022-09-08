9d2d6f09f91a73527675d2a5e5ffaebbade89ee3
Eshita95 <eshita.islam95@gmail.com>
2022-09-09 00:13:28 +0600

    api added

--- index.js
+++ index.js
@@ -3,10 +3,9 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require("dotenv").config();
-
+const port = process.env.PORT || 5000;
 
 const app = express();
-const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
